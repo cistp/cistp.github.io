160df519da1125b2d0bdfce1ec502627a9d9da16
BuildTools <unconfigured@null.spigotmc.org>
2020-12-11 15:47:41 -0800

    Update editing.js

--- js/editing.js
+++ js/editing.js
@@ -69,9 +69,7 @@ $('.createSchedule').click(function (e) {
                         classes.set('tutor', tutor);
                         objects.push(classes);
                     } else {
-                        alert("Time repeat!");
-                        location.reload();
-                        return;
+                        continue;
                     }
                 });
             }
