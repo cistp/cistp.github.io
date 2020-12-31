83b3342afe03e3483bd5b1d9f942ea219042acd6
BuildTools <unconfigured@null.spigotmc.org>
2020-12-30 20:04:16 -0800

    Update manage.js

--- js/manage.js
+++ js/manage.js
@@ -77,15 +77,6 @@ $(document).ready(function () {
             }
         }
     })
-    const query2 = new AV.Query('tuteeList');
-    query2.equalTo('isTutee', true);
-    query2.find().then((tutees) => {
-        for (let index = 0; index < tutees.length; index++) {
-            const element = tutees[index];
-            const comb2 = "<tr><td>" + element.get('name') +"</td><td>" + element.get('email') + '</td><td><button type="button" class="removeTutee btn btn-outline-primary" data-toggle="modal" data-target="#deleteTutee" value="' + element.id + '" tuteeName="' + element.get('name') + '"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg></button></td></tr>';
-            $('.listTutee').append(comb2);
-        }
-    })
     const query3 = new AV.Query('tutorList');
     query3.equalTo('isTutor', true);
     query3.find().then((tutors) => {
@@ -259,6 +250,15 @@ $('.tutee').click(function (e) {
     $('.userTab').hide();
     $('.calendarTab').hide();
     $(".tuteeTab").show();
+    const query = new AV.Query('tuteeList');
+    query.equalTo('isTutee', true);
+    query.find().then((tutees) => {
+        for (let index = 0; index < tutees.length; index++) {
+            const element = tutees[index];
+            const comb2 = "<tr><td>" + element.get('name') +"</td><td>" + element.get('email') + '</td><td><button type="button" class="removeTutee btn btn-outline-primary" data-toggle="modal" data-target="#deleteTutee" value="' + element.id + '" tuteeName="' + element.get('name') + '"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg></button></td></tr>';
+            $('.listTutee').append(comb2);
+        }
+    })
 });
 
 $('#form-register').submit(function (e) { 
