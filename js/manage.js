f56e8a0332069f2f2ba169f6f77b17bad6e7f0b5
BuildTools <unconfigured@null.spigotmc.org>
2021-01-09 14:38:17 -0800

    sche

--- js/manage.js
+++ js/manage.js
@@ -279,7 +279,7 @@ $('.tutor').click(function (e) {
     query.find().then((tutors) => {
         for (let index = 0; index < tutors.length; index++) {
             const tutor = tutors[index];
-            $('.Listtutor').append(`<tr><td>${tutor.get("user").id}</td><td>${tutor.get("tutorName")}</td><td>${tutor.get("volunteerHour")}</td><td><button type="button" class="removeTutor btn btn-outline-primary" data-toggle="modal" data-target="#deleteTutor" value="${tutor.id}" tutor="${tutor.get("tutorName")}"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg></button></td></tr>`);
+            $('.Listtutor').append(`<tr><td>${tutor.get("user").id}</td><td>${tutor.get("tutorName")}</td><td>${tutor.get("volunteerHour")}</td><td><button type="button" class="tutorSchedule btn btn-outline-primary ml-1" data-toggle="modal" data-target="#schechduleList" value="${tutor.get("user").id}" tutor="${tutor.get("tutorName")}"><i class="fa fa-calendar" aria-hidden="true"></i></button><button type="button" class="removeTutor btn btn-outline-primary ml-1" data-toggle="modal" data-target="#deleteTutor" value="${tutor.id}" tutor="${tutor.get("tutorName")}"><i class="fa fa-trash-o" aria-hidden="true"></i></button></td></tr>`);
         }
     });
 });
@@ -492,6 +492,20 @@ $('.listTutee').on('click', '.removeTutee',function () {
         }, 500);
       });
 });
+
+$('.Listtutor').on('click', '.tutorSchedule',function () {
+    $('.listClasses').html("");
+    $('.tutorScheduleTitle').html($(this).attr('tutor'));
+    const tutor = AV.Object.createWithoutData("_User", $(this).val());
+    const query = new AV.Query('Classes');
+    query.equalTo('tutor', tutor);
+    query.find().then((listClasses) => {
+        for(var i = 0, len = listClasses.length; i < len; i++){
+            let comb = "<tr><td>" + listClasses[i].attributes.date +"</td><td>" + listClasses[i].attributes.startTime + '</td><td>' + listClasses[i].attributes.tutee + '</td></tr>';
+            $('.listClasses').append(comb);
+        }
+    })
+})
 
 $('#form-tuteeRegister').submit(function (e) { 
     e.preventDefault();
